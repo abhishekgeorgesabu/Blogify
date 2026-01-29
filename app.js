8bfa2dc0a6fba9d2a0904af28559092abc284acb
Abhishek George Sabu <abhishekgeorgesabu@gmail.com>
2026-01-30 00:10:37 +0530

    Connected to Homepage

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const path = require("path");
 const connectMongoDb = require("./connection");
 
 connectMongoDb("mongodb://127.0.0.1:27017/Blogify");
+
 const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+	return res.render("homepage");
+});
 
 const PORT = 8000;
 app.listen(PORT, () => {
