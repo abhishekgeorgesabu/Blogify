e0a2de407f8448b3866a8c51510dd71db2fefb12
Abhishek George Sabu <abhishekgeorgesabu@gmail.com>
2026-02-01 00:43:14 +0530

    added the updated app.js till the current update

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require("express");
 const path = require("path");
 const connectMongoDb = require("./connection");
+const cookieParser = require("cookie-parser");
 
-connectMongoDb("mongodb://127.0.0.1:27017/Blogify");
+connectMongoDb("mongodb://127.0.0.1:27017/blogify");
+
+const userRoute = require("./routes/user");
+const { checkAuth } = require("./middleware/auth");
 
 const app = express();
-
-app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+app.use(checkAuth);
+
 app.get("/", (req, res) => {
 	return res.render("homepage");
 });
+
+app.use("/user", userRoute);
 
 const PORT = 8000;
 app.listen(PORT, () => {
